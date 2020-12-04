8a4cc01ade2e0ad8923cc5f34a3aebe58daec0fe
sharad1233 <thapasharad333@gmail.com>
2020-12-04 12:15:48 +0530

    nice

--- THAPAVC/THAPAVC/ViewController.swift
+++ THAPAVC/THAPAVC/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("sure")
+        print("suresdfdsfff")
     }
     func callWeb(){
         var len : Int = 310
